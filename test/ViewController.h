687360612556f97cc64d2d2ae0c2549cceac2762
纪宇伟 <jiyuwei@jiyuweideMacBook-Pro.local>
2017-06-05 13:28:49 +0800

    Initial Commit

---
+++ test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by 纪宇伟 on 2017/6/5.
+//  Copyright © 2017年 纪宇伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  test
+//
+//  Created by 纪宇伟 on 2017/6/5.
+//  Copyright © 2017年 纪宇伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
